e2b4375767069d5cd535eadaa55208f59b8b97f8
Samuel Rivera <samuel.r.rivera@gmail.com>
2011-07-04 19:38:41 -0400

    Small changes, and new files

--- olympus/device_olympus.mk
+++ olympus/device_olympus.mk
@@ -1,33 +1,3 @@
-#
-# Copyright (C) 2009 The Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-#
-# This is the product configuration for a generic GSM passion,
-# not specialized for any geography.
-#
-
-# The gps config appropriate for this device
-$(call inherit-product, device/common/gps/gps_us_supl.mk)
-
-## (1) First, the most specific values, i.e. the aspects that are specific to GSM
-
-## (3)  Finally, the least specific parts, i.e. the non-GSM-specific aspects
-
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
